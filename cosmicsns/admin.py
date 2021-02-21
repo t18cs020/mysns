9ce5543a40fcddedeef092189fd0910ab7eac174
KoheiKuwahata <t18cs020@yamanashi.ac.jp>
2021-02-21 20:06:58 +0900

    修正

--- cosmicsns/admin.py
+++ cosmicsns/admin.py
@@ -1,26 +1,17 @@
 from django.contrib import admin
 from .models import Posts
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Department
-from django.utils.translation import gettext, gettext_lazy as _
+from .models import User
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 admin.site.register(Posts)
 
-@admin.register(Department)
-class AdminDepartment(admin.ModelAdmin):
-    pass
+class UserInfoAdmin(admin.ModelAdmin):
 
-@admin.register(User)
-class AdminUserAdmin(UserAdmin):
+    def icon_image(self, obj):
+        return mark_safe('<img src="{}" style="width:80px;height:auto;">'.format(obj.user_icon.url))
 
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('full_name', 'email','departments')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    list_display = ('username', 'email', 'full_name', 'is_staff')
-    search_fields = ('username', 'full_name', 'email')
-    filter_horizontal = ('groups', 'user_permissions','departments')
+    list_display = ('username', 'icon_image')
+    
+admin.site.register(User, UserAdmin)
--- cosmicsns/models.py
+++ cosmicsns/models.py
@@ -1,60 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Department(models.Model):
-    """所属 兼任可"""
-
-    name = models.CharField(_('所属'), max_length=150, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = _('所属')
-        verbose_name_plural = _('所属')
-        
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    icon = models.ImageField(blank=True, null=True)
-    introduction = models.CharField(max_length=75, blank=True, null=True)
-    followers = models.ManyToManyField('self', blank=True, symmetrical=False)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    full_name = models.CharField(_('氏名'), max_length=150, blank=True)
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_(
-            'Designates whether the user can log into this admin site.'),
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    departments = models.ManyToManyField(
-        Department,
-        verbose_name=_('所属'),
-        blank=True,
-        help_text=_('Specific Departments for this user.'),
-        related_name="user_set",
-        related_query_name="user",
-    )
-    date_joined = models.DateTimeField(default=timezone.now)
-    objects = UserManager()
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+    icon = models.ImageField(upload_to='profiles/',blank=True, null=True)
 
 class Posts(models.Model):
     message = models.CharField(max_length=200)
